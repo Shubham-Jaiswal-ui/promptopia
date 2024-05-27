d4c2c0d2369250b50784912c4f48c8c10ad41065
unknown <shubham51jaiswal@gmail.com>
2024-05-28 02:08:35 +0530

    typo fixed

--- utils/database.js
+++ utils/database.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 let isConnected = false;
 export const connectToDb = async () => {
 	mongoose.set("strictQuery", true);
-	if (!isConnected) {
-		console.log("Mongo DB is already connected");
+	if (isConnected) {
+		console.log("MongoDB is already connected");
+		return;
 	}
 	try {
 		await mongoose.connect(process.env.MONGODB_URI, {
